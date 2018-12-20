3ee328b02348e3ff3b2e7c4614e08c0f593387e9
Huynh Ha <huynhsamha@gmail.com>
2018-12-21 05:42:57 +0700

    fix connect mongo

--- server/config/connect-mongo.js
+++ server/config/connect-mongo.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 import dbConfig from './db';
 
-mongoose.connect(dbConfig.uriMongo, {}, (err) => {
+mongoose.connect(dbConfig.uriMongo, { useNewUrlParser: true }, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('Connect mongo ok');
+    console.log('Connect to MongoDB successfully');
   }
 });
--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
 export default {
-  'uriMongo': process.env.URI_MONGO || 'mongodb://localhost/dontpad'
+  'uriMongo': process.env.URI_MONGO || 'mongodb://localhost:27017/dontpad'
 };
